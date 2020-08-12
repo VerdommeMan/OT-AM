24471fe2ec2c27db5d337d8404d9d3119d1e99c1
VerdommeMan <VerdommeMan1717@gmail.com>
2020-08-12 21:43:44 +0200

    Add module functions, replace getChars with a table and events

--- src/module/init.lua
+++ src/module/init.lua
@@ -28,10 +28,13 @@ local metamethods = {
     __len = true
 }
 
---settings
+--settings, can be changed directly instead of using the setters
 module.Settings = {}
 module.Settings.Heartbeat = 5 --max 60
 module.Settings.ExtraHumanoids = {} -- add models here that also need to be tracked next to the player, the model needs a PrimaryPart (this part will be tracked)
+
+--settings, cant be changed directly
+module.Settings.AutoAddPlayersCharacter = true -- if this is set to false then the person must manually add the player characters he wants to track
 
 -- other stuff
 local Areas = {} -- a list of areas
@@ -41,7 +44,7 @@ function module.addArea(uniqueName, ...)
     if Areas[uniqueName] then
         error("That name already exists")
     else
-        local self = setmetatable({}, Areas)
+        local self = setmetatable({}, Areas) -- mt allows ppl to access other areas from this table
         self.Area = Area.new(...)
         self.onEnter = Instance.new("BindableEvent")
         self.onLeave = Instance.new("BindableEvent")
@@ -52,22 +55,52 @@ function module.addArea(uniqueName, ...)
 end
 
 
--- function module.removeArea(uniqueName)
---     
--- end
-
-local function getTableChars()
-    local chars = {}
-    for _, player in pairs(Players:GetPlayers()) do
-        if player.Character then
-            table.insert(chars, player.Character)
-        end
+function module.removeArea(identifier)
+    if Areas[identifier] then
+        Areas[identifier] = nil
+    else
+        error("That key doesnt exist or is already removed")
     end
-    return chars
 end
 
+function module.getArea(indentifier)
+    return Areas[indentifier]
+end
+
+local function isValidChar(char)
+    return char and typeof(char) == "Instance" and char:IsA("Model") and char.PrimaryPart
+end
+
+function module.addChar(char) -- must be a model with a PrimaryPart set
+    if isValidChar(char) then
+        table.insert(module.Settings.ExtraHumanoids, char)
+    else
+        error("Given paramter must be a model with a set PrimaryPart")
+    end
+end
+
+function module.removeChar(char)
+    if char and table.find(module.Settings.ExtraHumanoids, char) then
+        table.remove(module.Settings.ExtraHumanoids,table.find(module.Settings.ExtraHumanoids, char))       
+    else
+        error("The char doesnt exist in the table") 
+    end
+    
+end
+
+local playerChars = {}
+
+Players.PlayerAdded:Connect(function(player)
+    player.CharacterAdded:Connect(function(character)
+        table.insert(playerChars, character)
+    end)
+    player.CharacterRemoving:Connect(function(character)
+        table.remove(playerChars, table.find(playerChars, character))
+    end)
+end)
+
 local function coreLoop()
-    for _, char in ipairs({table.unpack(getTableChars()), table.unpack(module.Settings.ExtraHumanoids)}) do
+    for _, char in ipairs({table.unpack(playerChars), table.unpack(module.Settings.ExtraHumanoids)}) do
         for key, area in pairs(Areas) do
             if not metamethods[key] and not area.chars[char] and area.Area:isInArea(char.PrimaryPart.Position)  then --ignore metamethods
                 area.onEnter:Fire(char)
